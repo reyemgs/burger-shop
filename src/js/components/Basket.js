0ab7eb026a4e1773eb78757e198eaf4245b0364d
reyemgs <thetortyan@gmail.com>
2020-10-27 16:44:07 +0300

    product cards & basket update

--- src/js/components/Basket.js
+++ src/js/components/Basket.js
@@ -1,25 +1,40 @@
 export default class Basket {
     constructor() {
         this.sideBar = document.querySelector('#sidebar-wrapper');
+        this.totalPrice = 0;
     }
 
     createBasket() {
         const basketWrapper = document.createElement('div');
-        const basketHeader = document.createElement('span');
-        const basketBody = document.createElement('div');
-        const basketOrderButton = document.createElement('button');
-
         basketWrapper.className = 'basket';
-        basketHeader.className = 'basket-header';
-        basketBody.className = 'basket-body';
-        basketOrderButton.className = 'order-button';
 
+        const basketHeader = document.createElement('span');
+        basketHeader.className = 'basket-header';
         basketHeader.innerHTML = '<i class="fas fa-shopping-basket"></i> Корзина';
+
+        const basketBody = document.createElement('div');
+        basketBody.className = 'basket-body';
+
+        const basketLabelWrapper = document.createElement('div');
+        basketLabelWrapper.className = 'basket-label-wrapper';
+        const basketNameLabel = document.createElement('span');
+        basketNameLabel.className = 'basket-name-label';
+        const basketQuantityLabel = document.createElement('span');
+        basketQuantityLabel.className = 'basket-quantity-label';
+        basketNameLabel.innerHTML = 'Название';
+        basketQuantityLabel.innerHTML = 'Количество';
+
+        const basketTotalPrice = document.createElement('span');
+        basketTotalPrice.className = 'basket-total-price';
+        basketTotalPrice.innerHTML = `Итого: ${this.totalPrice} руб.`;
+
+        const basketOrderButton = document.createElement('button');
+        basketOrderButton.className = 'order-button';
         basketOrderButton.innerHTML = 'ОФОРМИТЬ ЗАКАЗ';
 
-        basketBody.append(basketOrderButton);
-        basketWrapper.append(basketHeader);
-        basketWrapper.append(basketBody);
+        basketLabelWrapper.append(basketNameLabel, basketQuantityLabel);
+        basketBody.append(basketLabelWrapper, basketTotalPrice, basketOrderButton);
+        basketWrapper.append(basketHeader, basketBody);
         this.sideBar.append(basketWrapper);
     }
 
--- src/js/components/ProductCard.js
+++ src/js/components/ProductCard.js
@@ -1,22 +1,24 @@
 export default class ProductCard {
     constructor(item, response) {
         this.rightSideWrapper = document.querySelector('#rightside-wrapper');
+        this.response = response;
 
         this.name = item.name;
+        this.image = item.image;
+        this.price = item.price;
         this.description = item.description;
         this.market = item.market;
-        this.image = item.image;
 
-        this.response = response;
+        this.productQuantity = 1;
     }
 
     createProductCard() {
         const productCardWrapper = document.createElement('div');
         productCardWrapper.className = 'product-card-wrapper';
 
-        const productCardMarket = document.createElement('img');
-        productCardMarket.className = 'product-market';
-        productCardMarket.setAttribute(
+        const productMarket = document.createElement('img');
+        productMarket.className = 'product-market';
+        productMarket.setAttribute(
             'src',
             `../examples${this.response.markets[this.market].image}`
         );
@@ -33,10 +35,67 @@ export default class ProductCard {
         productDescription.className = 'product-description';
         productDescription.innerHTML = this.description;
 
-        productCardWrapper.append(productCardMarket);
-        productCardWrapper.append(productImage);
-        productCardWrapper.append(productName);
-        productCardWrapper.append(productDescription);
+        const productPrice = document.createElement('span');
+        productPrice.className = 'product-price';
+        productPrice.innerHTML = `Цена: ${this.price} руб.`;
+
+        const productSetAmountWrapper = document.createElement('div');
+        const productAmountLabel = document.createElement('span');
+        const productAmount = document.createElement('span');
+        const productIncreaseButton = document.createElement('div');
+        const productDecreaseButton = document.createElement('div');
+        const productInBasketButton = document.createElement('button');
+
+        productSetAmountWrapper.className = 'set-amount-wrapper';
+        productAmountLabel.className = 'product-amount-label';
+        productAmount.className = 'product-amount';
+        productIncreaseButton.className = 'increase-button';
+        productDecreaseButton.className = 'decrease-button';
+        productInBasketButton.className = 'in-basket-button';
+
+        productAmountLabel.innerHTML = 'Количество';
+        productAmount.innerHTML = this.productQuantity;
+        productIncreaseButton.innerHTML = '<i class="fas fa-plus-circle"></i>';
+        productDecreaseButton.innerHTML = '<i class="fas fa-minus-circle"></i>';
+        productInBasketButton.innerHTML = 'В КОРЗИНУ';
+
+        productSetAmountWrapper.append(
+            productAmountLabel,
+            productDecreaseButton,
+            productAmount,
+            productIncreaseButton,
+            productInBasketButton
+        );
+        productCardWrapper.append(
+            productMarket,
+            productImage,
+            productName,
+            productDescription,
+            productPrice,
+            productSetAmountWrapper
+        );
         this.rightSideWrapper.append(productCardWrapper);
+
+        // * EVENTS * //
+        productIncreaseButton.addEventListener('click', () =>
+            this.increaseQuantity(productAmount)
+        );
+
+        productDecreaseButton.addEventListener('click', () =>
+            this.decreaseQuantity(productAmount)
+        );
+    }
+
+    increaseQuantity(productAmount) {
+        this.productQuantity = this.productQuantity + 1;
+        productAmount.innerHTML = this.productQuantity;
+    }
+
+    decreaseQuantity(productAmount) {
+        if (this.productQuantity === 1) return;
+        else {
+            this.productQuantity = this.productQuantity - 1;
+            productAmount.innerHTML = this.productQuantity;
+        }
     }
 }
